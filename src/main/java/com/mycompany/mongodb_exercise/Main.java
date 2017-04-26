8c6f904779349dd0b7438de4fa0a3c4a373ef245
Unknown <ale.steen@hotmail.com>
2017-04-26 15:39:14 +0200

    Can now get user count!

--- src/main/java/com/mycompany/mongodb_exercise/Main.java
+++ src/main/java/com/mycompany/mongodb_exercise/Main.java
@@ -7,8 +7,11 @@ package com.mycompany.mongodb_exercise;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.DistinctIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import org.bson.BsonValue;
 import org.bson.Document;
 
 /**
@@ -16,14 +19,21 @@ import org.bson.Document;
  * @author alexander
  */
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("User Count: " + getUserCount());
+    }
+    
+    private static long getUserCount() {
         MongoClientURI connStr = new MongoClientURI("mongodb://localhost:27017");
         MongoClient mongoClient = new MongoClient(connStr);
 
         MongoDatabase db = mongoClient.getDatabase("social_net");
-        MongoCollection<Document> collection = db.getCollection("tweets");
-
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        MongoCollection<Document> col = db.getCollection("tweets");
+        
+        ArrayList<BsonValue> dis = col.distinct("user", BsonValue.class)
+                .into(new ArrayList<>());
+        
+        return dis.size();
     }
 }
